1592c44af01a73e77c1697c37e54d6db34b241ba
Carlos Caceres <ccaceres461@gmail.com>
2022-10-25 22:12:14 -0400

    Limpieza de código Se agrego un Get basico de ejemplo

---
+++ ApiAutores/Controllers/AutoresController.cs
@@ -0,0 +1,22 @@
+﻿using ApiAutores.Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiAutores.Controllers
+{
+    [ApiController] // Permite hacer validaciones automaticas
+    [Route("api/autores")] // Ruta base de controlador
+    public class AutoresController: ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>()
+            { 
+                new Autor(){Id = 1, Nombre = "Autor 1"  },
+                new Autor(){Id = 2, Nombre = "Autor 2"  },
+            };
+                ;
+        }
+
+    }
+}
---
+++ ApiAutores/Entidades/Autor.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiAutores.Entidades
+{
+    public class Autor
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+    }
+}
